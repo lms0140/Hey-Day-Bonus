cdf3d9501a4364054f1b4a6e659001e86b4febd4
lms0140 <145612126+lms0140@users.noreply.github.com>
2023-10-17 20:15:50 -0500

    Add files via upload

---
+++ heyday.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#whats pops up after excecuting heyday.sh file
+echo "Good Evening, What's your name?"
+
+#has to read the input to beable to use as output
+read name
+
+#After reading input it outputs:
+
+echo "Hey, $name"
+echo "At Auburn University, we celebrate Hey Day.Which dates all the way back to 1947,we make sure everyone feels they belong by saying hey to them!"
+
